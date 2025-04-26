59976aa9ebea3af8cff7719d36a866d6edf8d006
wptasmina <wptasmina@gmail.com>
2025-04-26 21:23:13 +0600

    banner text short

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -30,15 +30,12 @@ export default function Banner() {
       className="mySwiper "
     >
       <SwiperSlide>
-        <section className='relative bg-bannerImg bg-no-repeat w-full h-auto object-fill bg-right-top' >
+        <section className='relative bg-bannerImg bg-no-repeat bg-cover bg-center w-full h-auto object-fill' >
           <div class="w-full h-[400px] object-cover flex justify-center items-center bg-blackOvrtlay">
             <div class="p-2 flex justify-center items-center flex-col gap-4"> 
               <h1 className='text-white md:text-4xl text-2xl font-bold text-center'>Smart Asset Managment for HR Excellence</h1>
               <p className='text-white/80 font-bold md:text-lg text-md md:w-2/3 mx-auto px-2 text-center tracking-wider'>
-              TakeSmart simplifies asset tracking and allocation for HR
-                managers. Efficiently manage returnable and non-returnable
-                items, streamline processes, and keep your workforce organized
-                with ease.
+              TakeSmart streamlines asset tracking and allocation for HR managers, making it easy to manage items and keep the workforce organized.
               </p>
                 <div className='border-2 border-orange-700 rounded-md p-1'> 
                 <button
@@ -59,7 +56,7 @@ export default function Banner() {
           <div class="w-full h-[400px] flex justify-center items-center bg-blackOvrtlay">
             <div class="p-2 flex justify-center items-center flex-col gap-4"> 
               <h1 className='text-white md:text-4xl text-2xl font-bold text-center'>Stay productive and organized with TakeSmart!</h1>
-              <p className='text-white/80 font-bold md:text-lg text-md md:w-2/3 mx-auto px-2 text-center tracking-wider'>Seamlessly request, track, and manage company-provided assets with ease. Say goodbye to confusion and enjoy a hassle-free experience, ensuring every employee gets what they need—efficiently and effortlessly.</p>
+              <p className='text-white/80 font-bold md:text-lg text-md md:w-2/3 mx-auto px-2 text-center tracking-wider'>Easily request, track, and manage company assets—no confusion, just a smooth, hassle-free experience.</p>
                 <div className='border-2 border-orange-700 rounded-md p-1'> 
                 <button
                     className="px-8 text-white py-2 bg-[#0c2757] rounded-md tracking-wider"
