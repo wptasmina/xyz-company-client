25f07f5b4585e59684f4ce4a3c9a5c8425223776
wptasmina <wptasmina@gmail.com>
2025-04-27 01:19:27 +0600

    featres page update

--- src/Page/ProductFeatur/ProductFeatures.jsx
+++ src/Page/ProductFeatur/ProductFeatures.jsx
@@ -30,87 +30,91 @@ const benefits = [
 ];
 
 const ProductFeatures = () => {
-    return (
-      <>
+  return (
+    <>
       <Helmet >
         <title>TrakSmart | Contact </title>
       </Helmet>
 
       <Navbar />
-    <div className="bg-white dark:bg-[#161f2c] max-w-6xl mx-auto p-6 mt-10">
-        {/* Title & Description */}
-        <div data-aos="fade-up"
-             ata-aos="zoom-in-left"
-            data-aos-duration="1000" 
-            className="text-center mb-12">
-          <h2 data-aos="fade-up"
-            ata-aos="zoom-in-left"
-            data-aos-duration="1000" 
-            className="dark:text-white text-4xl font-bold text-[#031278] ">Why Choose Our Asset Management System?</h2>
-          <p data-aos="fade-up"
-            ata-aos="zoom-in-left"
-            data-aos-duration="1000" 
-            className="text-gray-600 mt-2 text-lg">
-            Manage and track your company's assets effortlessly with our feature-rich solution.
-          </p>
-        </div>
-  
-        {/* Features Section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {features.map((feature, index) => (
-            <div key={index} className="flex items-center bg-gray-100 p-4 rounded-lg shadow-sm">
-              <div data-aos="fade-right"
-                    ata-aos="zoom-in-left"
-                    data-aos-duration="1000" 
+      <div className="bg-white dark:bg-[#161f2c] pt-10">
 
-                    className="text-[#131c58] dark:text-[#131c58] text-2xl mr-3">
-                      {feature.icon}
+        <div className="w-10/12 mx-auto">
+          {/* Title & Description */}
+          <div data-aos="fade-up"
+            ata-aos="zoom-in-left"
+            data-aos-duration="1000"
+            className="text-center mb-12">
+            <h2 data-aos="fade-up"
+              ata-aos="zoom-in-left"
+              data-aos-duration="1000"
+              className="dark:text-white md:text-4xl text-2xl font-bold text-[#031278] md:w-2/3 mx-auto w-full">Features Of Our Asset Management System?</h2>
+            <p data-aos="fade-up"
+              ata-aos="zoom-in-left"
+              data-aos-duration="1000"
+              className="text-gray-600 dark:text-gray-500 mt-2 text-lg">
+              Manage and track your company's assets effortlessly with our feature-rich solution.
+            </p>
+          </div>
+
+          {/* Features Section */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
+            {features.map((feature, index) => (
+              <div key={index} className="flex items-center bg-gray-100 dark:bg-[#0d1520] p-4 rounded-lg shadow-sm">
+                <div data-aos="fade-right"
+                  ata-aos="zoom-in-left"
+                  data-aos-duration="1000"
+
+                  className="text-[#131c58] dark:text-orange-700 text-2xl mr-3">
+                  {feature.icon}
                 </div>
-              <div>
-                <h3 data-aos="fade-up"
-                        ata-aos="zoom-in-left"
-                        data-aos-duration="1000" className="text-lg dark:text-[#131c58] font-semibold">{feature.title}</h3>
-                <p data-aos="fade-up"
+                <div>
+                  <h3 data-aos="fade-up"
                     ata-aos="zoom-in-left"
-                    data-aos-duration="1000" className="text-gray-600 text-sm">
+                    data-aos-duration="1000" className="text-lg dark:text-orange-700 font-semibold">{feature.title}</h3>
+                  <p data-aos="fade-up"
+                    ata-aos="zoom-in-left"
+                    data-aos-duration="1000" className="text-gray-600 dark:text-gray-400 text-sm">
                     {feature.description}
                   </p>
+                </div>
               </div>
+            ))}
+          </div>
+
+          {/* Business Benefits section */}
+          <section className="mt-12 text-center">
+            {/* Title & Description */}
+            <h3 data-aos="fade-up"
+              ata-aos="zoom-in-left"
+              data-aos-duration="1000" className="dark:text-white text-3xl font-bold text-[#131c58]">Business Benefits</h3>
+            <p data-aos="fade-up"
+              ata-aos="zoom-in-left"
+              data-aos-duration="1000" className="text-gray-600 dark:text-gray-400 mt-2 text-lg">
+              Unlock the advantages of smarter asset management.
+            </p>
+
+            {/* Benefits Grid */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 py-6">
+              {benefits.map((benefit, index) => (
+                <div
+                  key={index}
+                  className="flex items-center bg-[#e6e8fc] dark:bg-[#0d1520]  p-4 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
+                >
+                  <FaCheckCircle className="text-[#031278] dark:text-orange-700 mr-3 text-xl" />
+                  <span className="text-gray-700 dark:text-gray-400">{benefit}</span>
+                </div>
+              ))}
             </div>
-          ))}
+          </section>
         </div>
 
-        {/* Business Benefits section */}
-    <section className="mt-12 text-center">
-      {/* Title & Description */}
-      <h3 data-aos="fade-up"
-             ata-aos="zoom-in-left"
-            data-aos-duration="1000"  className="dark:text-white text-3xl font-bold text-[#131c58]">Business Benefits</h3>
-      <p data-aos="fade-up"
-             ata-aos="zoom-in-left"
-            data-aos-duration="1000"  className="text-gray-600 mt-2 text-lg">
-        Unlock the advantages of smarter asset management.
-      </p>
-
-      {/* Benefits Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 my-6">
-        {benefits.map((benefit, index) => (
-          <div
-            key={index}
-            className="flex items-center bg-[#e6e8fc] p-4 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
-          >
-            <FaCheckCircle className="text-[#031278] mr-3 text-xl" />
-            <span className="text-gray-700">{benefit}</span>
-          </div>
-        ))}
-      </div>
-    </section>
       </div>
 
-    <Footer />
-      
-      </>
-    );
-  };
-  
-  export default ProductFeatures;
+      <Footer />
+
+    </>
+  );
+};
+
+export default ProductFeatures;
